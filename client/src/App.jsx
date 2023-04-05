c105e1956de93043acf045fbe5aafb04a24bc46e
elainealso <eabargiela@gmail.com>
2023-04-05 15:01:12 +0200

    navbar-elaine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import NavbarComp from "./components/NavbarComp";
 import "./styles/App.scss";
 
 function App() {
   return <div className="App">
+    <NavbarComp/>
     <h1>RescueUs</h1>
+
   </div>;
 }
 
